85bbee1127686b4dd04fd8c2bd6707d963c2d63a
Drew Dabbs <dabbs.drew@gmail.com>
2021-11-15 18:45:11 -0500

    initial commit

---
+++ IntroToAPI/Swapi.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapi_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = httpClient.GetAsync("https://swapi.dev/api/people/1/").Result;
+            
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            Person personResponse = response.Content.ReadAsAsync<Person>().Result;
+            Console.WriteLine(personResponse.Name);
+            Console.WriteLine(personResponse.Created);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IntroToAPI/Swapi.Console/Services/SwapiService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapi_Console
+{
+    public class SwapiService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        public async Task<Person> GetPersonAsync(string url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Person person = await response.Content.ReadAsAsync<Person>();
+                return person;
+            }
+            return null;
+        }
+
+        public async Task<T> GetAsAsync<T>(string url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<T>();
+            }
+            return default;
+        }
+    }
+
+
+}
